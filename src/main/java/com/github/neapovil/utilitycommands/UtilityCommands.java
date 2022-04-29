6b128a6774e6b563fdee2d1de0c5cff6bad5c8e5
Nearata <nearata@protonmail.com>
2022-04-29 10:19:26 +0200

    init project
    * /heal [player]

---
+++ src/main/java/com/github/neapovil/utilitycommands/UtilityCommands.java
@@ -0,0 +1,28 @@
+package com.github.neapovil.utilitycommands;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+import com.github.neapovil.utilitycommands.command.HealCommand;
+
+public final class UtilityCommands extends JavaPlugin
+{
+    private static UtilityCommands instance;
+
+    @Override
+    public void onEnable()
+    {
+        instance = this;
+
+        HealCommand.register();
+    }
+
+    @Override
+    public void onDisable()
+    {
+    }
+
+    public static UtilityCommands getInstance()
+    {
+        return instance;
+    }
+}
---
+++ src/main/java/com/github/neapovil/utilitycommands/command/HealCommand.java
@@ -0,0 +1,35 @@
+package com.github.neapovil.utilitycommands.command;
+
+import org.bukkit.attribute.Attribute;
+import org.bukkit.entity.Player;
+
+import dev.jorel.commandapi.CommandAPICommand;
+import dev.jorel.commandapi.arguments.PlayerArgument;
+
+public final class HealCommand
+{
+    public static final void register()
+    {
+        new CommandAPICommand("heal")
+                .withPermission("utilitycommands.command.heal")
+                .executesPlayer((player, args) -> {
+                    player.setHealth(player.getAttribute(Attribute.GENERIC_MAX_HEALTH).getValue());
+
+                    player.sendMessage("You have been healed");
+                })
+                .register();
+
+        new CommandAPICommand("heal")
+                .withPermission("utilitycommands.command.heal.other")
+                .withArguments(new PlayerArgument("player"))
+                .executes((sender, args) -> {
+                    final Player player = (Player) args[0];
+
+                    player.setHealth(player.getAttribute(Attribute.GENERIC_MAX_HEALTH).getValue());
+                    player.sendMessage("You have been healed");
+
+                    sender.sendMessage(player.getName() + " has been healed");
+                })
+                .register();
+    }
+}
